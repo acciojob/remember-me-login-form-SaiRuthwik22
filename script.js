0be95b8f994892c917746081854baa458e890848
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-22 11:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function check() {
 	   if(localStorage.getItem("https://www.svgrepo.com/show/345221/three-dots.svgusername")){
         let existing = document.getElementById("existing");
-		   let form = document.getElementById("form")
-        if (existing) {
+        if (!existing) {
             button = document.createElement("button");
             button.setAttribute("id","existing")
             button.textContent = "Login as existing user";
