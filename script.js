76443627e1b2802f849b76423cc8c155a081e431
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-08-29 21:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function check() {
         }
     } else {
         if (button) {
-            button.remove();
+            console.log("true")
         }
     }
 }
@@ -32,13 +32,25 @@ function execute(event) {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let form = document.getElementById("form");
+	let button = document.getElementById("existing");
 
     if(checkbox.checked) {
         localStorage.setItem("username",username);
         localStorage.setItem("password",password);
+		button = document.createElement("button");
+        button.setAttribute("id","existing");
+        button.textContent = "Login as existing user";
+        button.addEventListener("click", function() {
+                alert("Logged in as " + localStorage.getItem("username"));
+            });
+		form.appendChild(button)
+		alert("Logged in as " + localStorage.getItem("username"))
+		
     } else {
-        localStorage.clear();
+		console.log("removing")
+        localStorage.getItem('username') && localStorage.removeItem('username');
+		localStorage.getItem('password') && localStorage.removeItem('password');
+		button && button.remove()
+		alert("Logged in as " + username);
     }
-    check();
-    alert("Logged in as " + username);
 }
